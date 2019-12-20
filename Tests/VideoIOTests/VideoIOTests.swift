fa3691e898b0af5e4dc5c76352d1dd25411daff1
YuAo <me@imyuao.com>
2019-12-20 12:41:46 +0800

    audio/video settings

--- Tests/VideoIOTests/VideoIOTests.swift
+++ Tests/VideoIOTests/VideoIOTests.swift
@@ -1,5 +1,6 @@
 import XCTest
 @testable import VideoIO
+import AVFoundation
 
 final class VideoIOTests: XCTestCase {
     func testExample() {
@@ -7,7 +8,16 @@ final class VideoIOTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         //XCTAssertEqual(VideoIO().text, "Hello, World!")
+        var audioSettings = AudioSettings(formatID: kAudioFormatMPEG4AAC, channels: 2, sampleRate: 44100)
+        audioSettings.bitRate = 300
+        dump(audioSettings.toDictionary())
         
+        if #available(iOS 11.0, *) {
+            var videoSettings = VideoSettings.hevc(videoSize: CGSize(width: 1920, height: 1080), averageBitRate: 5000 * 1000)
+            dump(videoSettings.toDictionary())
+        } else {
+            // Fallback on earlier versions
+        }
     }
 
     static var allTests = [
