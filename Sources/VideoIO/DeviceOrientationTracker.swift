72f20933b30dcde7fb909c0fc8a169a1bd06112c
YuAo <me@imyuao.com>
2019-12-30 14:15:09 +0800

    update DeviceOrientationTracker api

--- Sources/VideoIO/DeviceOrientationTracker.swift
+++ Sources/VideoIO/DeviceOrientationTracker.swift
@@ -41,7 +41,87 @@ public class DeviceOrientationTracker {
         motionManager.magnetometerUpdateInterval = 0.33
     }
     
-    func start() {
+    private func handle(motionData: CMDeviceMotion) {
+        let acceleration = motionData.gravity
+        let xx = -acceleration.x
+        let yy = acceleration.y
+        let z = acceleration.z
+        let angle = atan2(yy, xx)
+        var deviceOrientation = _deviceOrientation
+        let absoluteZ = fabs(z)
+        
+        if deviceOrientation == .faceUp || deviceOrientation == .faceDown {
+            if absoluteZ < 0.845 {
+                if angle < -2.6 {
+                    deviceOrientation = .landscapeRight
+                } else if angle > -2.05 && angle < -1.1 {
+                    deviceOrientation = .portrait
+                } else if angle > -0.48 && angle < 0.48 {
+                    deviceOrientation = .landscapeLeft
+                } else if angle > 1.08 && angle < 2.08 {
+                    deviceOrientation = .portraitUpsideDown
+                }
+            } else if z < 0 {
+                deviceOrientation = .faceUp
+            } else if z > 0 {
+                deviceOrientation = .faceDown
+            }
+        } else {
+            if z > 0.875 {
+                deviceOrientation = .faceDown
+            } else if z < -0.875 {
+                deviceOrientation = .faceUp
+            } else {
+                switch deviceOrientation {
+                case .landscapeLeft:
+                    if angle < -1.07 {
+                        deviceOrientation = .portrait
+                    }
+                    if angle > 1.08 {
+                        deviceOrientation = .portraitUpsideDown
+                    }
+                case .landscapeRight:
+                    if angle < 0 && angle > -2.05 {
+                        deviceOrientation = .portrait
+                    }
+                    if angle > 0 && angle < 2.05 {
+                        deviceOrientation = .portraitUpsideDown
+                    }
+                case .portraitUpsideDown:
+                    if angle > 2.66 {
+                        deviceOrientation = .landscapeRight
+                    }
+                    if angle < 0.48 {
+                        deviceOrientation = .landscapeLeft
+                    }
+                case .portrait:
+                    if angle > -0.47 {
+                        deviceOrientation = .landscapeLeft
+                    }
+                    if angle < -2.64 {
+                        deviceOrientation = .landscapeRight
+                    }
+                default:
+                    if angle > -0.47 {
+                        deviceOrientation = .landscapeLeft
+                    }
+                    if angle < -2.64 {
+                        deviceOrientation = .landscapeRight
+                    }
+                }
+            }
+        }
+        
+        if _deviceOrientation != deviceOrientation {
+            _deviceOrientation = deviceOrientation
+            DispatchQueue.main.async {
+                self.deviceOrientation = deviceOrientation
+                self.delegate?.deviceOrientationUpdated(tracker: self, orientation: deviceOrientation)
+            }
+        }
+    }
+    
+    public func start() {
         guard motionManager.isDeviceMotionAvailable else {
             assertionFailure()
             return
@@ -50,88 +130,17 @@ public class DeviceOrientationTracker {
             guard let strongSelf = self, let motionData = motionData else {
                 return
             }
-            
-            let acceleration = motionData.gravity
-            let xx = -acceleration.x
-            let yy = acceleration.y
-            let z = acceleration.z
-            let angle = atan2(yy, xx)
-            var deviceOrientation = strongSelf._deviceOrientation
-            let absoluteZ = fabs(z)
-            
-            if deviceOrientation == .faceUp || deviceOrientation == .faceDown {
-                if absoluteZ < 0.845 {
-                    if angle < -2.6 {
-                        deviceOrientation = .landscapeRight
-                    } else if angle > -2.05 && angle < -1.1 {
-                        deviceOrientation = .portrait
-                    } else if angle > -0.48 && angle < 0.48 {
-                        deviceOrientation = .landscapeLeft
-                    } else if angle > 1.08 && angle < 2.08 {
-                        deviceOrientation = .portraitUpsideDown
-                    }
-                } else if z < 0 {
-                    deviceOrientation = .faceUp
-                } else if z > 0 {
-                    deviceOrientation = .faceDown
-                }
-            } else {
-                if z > 0.875 {
-                    deviceOrientation = .faceDown
-                } else if z < -0.875 {
-                    deviceOrientation = .faceUp
-                } else {
-                    switch deviceOrientation {
-                    case .landscapeLeft:
-                        if angle < -1.07 {
-                            deviceOrientation = .portrait
-                        }
-                        if angle > 1.08 {
-                            deviceOrientation = .portraitUpsideDown
-                        }
-                    case .landscapeRight:
-                        if angle < 0 && angle > -2.05 {
-                            deviceOrientation = .portrait
-                        }
-                        if angle > 0 && angle < 2.05 {
-                            deviceOrientation = .portraitUpsideDown
-                        }
-                    case .portraitUpsideDown:
-                        if angle > 2.66 {
-                            deviceOrientation = .landscapeRight
-                        }
-                        if angle < 0.48 {
-                            deviceOrientation = .landscapeLeft
-                        }
-                    case .portrait:
-                        if angle > -0.47 {
-                            deviceOrientation = .landscapeLeft
-                        }
-                        if angle < -2.64 {
-                            deviceOrientation = .landscapeRight
-                        }
-                    default:
-                        if angle > -0.47 {
-                            deviceOrientation = .landscapeLeft
-                        }
-                        if angle < -2.64 {
-                            deviceOrientation = .landscapeRight
-                        }
-                    }
-                }
+            strongSelf.handle(motionData: motionData)
+        }
+        queue.addOperation { [weak self] in
+            guard let strongSelf = self, let motionData = strongSelf.motionManager.deviceMotion else {
+                return
             }
-            
-            if strongSelf._deviceOrientation != deviceOrientation {
-                strongSelf._deviceOrientation = deviceOrientation
-                DispatchQueue.main.async {
-                    strongSelf.deviceOrientation = deviceOrientation
-                    strongSelf.delegate?.deviceOrientationUpdated(tracker: strongSelf, orientation: deviceOrientation)
-                }
-            }
+            strongSelf.handle(motionData: motionData)
         }
     }
     
-    func stop() {
+    public func stop() {
         motionManager.stopAccelerometerUpdates()
     }
 }
